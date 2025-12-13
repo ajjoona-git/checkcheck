f544edf388fd612297dd00f4658efd74edfb6fb1
ajjoona <ajjoona@gmail.com>
2025-12-14 01:29:09 +0900

    WIP: Navbar 작업 중 - Auth 기능 병합을 위한 임시 저장

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
     <h1>Moathon</h1>
-    <nav>
-      <RouterLink :to="{ name: 'home' }">HOME</RouterLink> | 
-      <RouterLink :to="{ name: 'explore' }">EXPLORE</RouterLink> | 
-      <RouterLink :to="{ name: 'create' }">CREATE</RouterLink> | 
-      <RouterLink :to="{ name: 'mypage' }">MY PAGE</RouterLink> | 
-      <RouterLink :to="{ name: 'login' }">LOGIN</RouterLink> | 
-      <RouterLink :to="{ name: 'signup' }">SIGNUP</RouterLink>
-    </nav>
+    <Navbar />
+
     <RouterView />
   </div>
 </template>
 
 <script setup>
   import { RouterView, RouterLink } from 'vue-router';
+  import Navbar from './components/common/Navbar.vue';
 </script>
 
 <style scoped>
