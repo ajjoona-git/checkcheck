9d697b470add81fa129f10dcc05f9078ffd311fa
ajjoona <ajjoona@gmail.com>
2025-12-13 18:27:58 +0900

    feat(FE): 회원가입 폼 라벨을 한글로 변경
    로그인 페이지와 통일성을 맞추기 위해 회원가입 시 데이터 라벨을 영문에서 한글로 변경함
    
    Refs #25

--- frontend/src/views/auth/SignupView.vue
+++ frontend/src/views/auth/SignupView.vue
@@ -3,22 +3,22 @@
     <h1>회원가입 페이지</h1>
 
     <form @submit.prevent="signUp">
-      <label for="username">username: </label>
+      <label for="username">이름: </label>
       <input type="text" id="username" v-model.trim="username" /> <br>
 
-      <label for="email">email: </label>
+      <label for="email">이메일: </label>
       <input type="email" id="email" v-model.trim="email" /> <br>
       
-      <label for="password1">password: </label>
+      <label for="password1">비밀번호: </label>
       <input type="password" id="password1" v-model.trim="password1" /> <br>
       
-      <label for="password2">password confirmation: </label>
+      <label for="password2">비밀번호 확인: </label>
       <input type="password" id="password2" v-model.trim="password2" /> <br>
       
-      <label for="nickname">nickname: </label>
+      <label for="nickname">닉네임: </label>
       <input type="text" id="nickname" v-model.trim="nickname" /> <br>
       
-      <label for="birth">birth: </label>
+      <label for="birth">생년월일: </label>
       <input type="date" id="birth" v-model.trim="birth" /> <br>
       
       <input type="submit" value="signup">
