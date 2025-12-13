4086705ff681a5befda0d5b34b7fc7bb0e5f50af
ajjoona <ajjoona@gmail.com>
2025-12-13 16:19:51 +0900

    feat(FE): 로그인 페이지 기능 구현

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
     <h1>로그인 페이지</h1>
+
+    <form @submit.prevent="logIn">
+      <label for="username">username: </label>
+      <input type="text" id="username" v-model.trim="username" /> <br>
+
+      <label for="email">email: </label>
+      <input type="email" id="email" v-model.trim="email" /> <br>
+
+      <label for="password">password: </label>
+      <input type="password" id="password" v-model.trim="password" /> <br>
+
+      <input type="submit" value="LogIn" />
+    </form>
   </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+  import { useAccountStore } from '@/stores/accounts';
 
+  const username = ref(null)
+  const email = ref(null)
+  const password = ref(null)
+
+  const accountStore = useAccountStore()
+
+  const logIn = function () {
+    const payload = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    }
+    accountStore.logIn(payload)
+  }
 </script>
 
 <style scoped>
