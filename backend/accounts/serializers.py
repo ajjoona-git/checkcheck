ddc00111983e1332a4d8155a874d4ec9bbb332aa
jhs <jhsjhs8566@gmail.com>
2025-12-13 15:15:51 +0900

    feat(BE): 비밀번호 찾기 및 재설정 API 구현

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
@@ -29,3 +30,38 @@ class CustomRegisterSerializer(RegisterSerializer):
         user.birth = self.validated_data.get("birth")
         user.save()
         return user
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        return value
+
+
+class UserPasswordResetConfirmSerializer(serializers.ModelSerializer):
+    new_password = serializers.CharField(write_only=True)
+    new_password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        # User 모델에서 비밀번호만 처리 
+        fields = ("new_password", "new_password2")
+
+    def validate(self, attrs):
+        pw1 = attrs.get("new_password")
+        pw2 = attrs.get("new_password2")
+
+        if pw1 != pw2:
+            raise serializers.ValidationError(
+                {"new_password2": "비밀번호가 일치하지 않습니다."}
+            )
+
+        validate_password(pw1, self.instance)
+        return attrs
+
+    def update(self, instance, validated_data):
+        new_password = validated_data.get['new_password']
+        instance.set_password(new_password)
+        instance.save()
+        return instance
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,7 @@
 from django,urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('find_password/', views.find_password),
+    path('reset_password/', views.reset_password),
+]
