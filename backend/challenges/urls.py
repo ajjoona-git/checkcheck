540461ae40d143164869ecc6a9a053d0dd23986d
Joona <ajjoona@gmail.com>
2025-12-22 09:13:43 +0900

    Fix URL patterns for moathon comments

--- backend/challenges/urls.py
+++ backend/challenges/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:moathon_pk>/', views.moathon_detail),
     path('create/', views.moathon_create),
 
-    path("moathons/<int:moathon_pk>/comments/", views.moathon_comment_list_create),
-    path("comments/<int:comment_pk>/", views.moathon_comment_detail),
+    path("<int:moathon_pk>/comments/", views.moathon_comment_list_create),
+    path("<int:moathon_pk>/comments/<int:comment_pk>/", views.moathon_comment_detail),
 ]
