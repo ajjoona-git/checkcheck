671a0d806c66af67e8f614f04f843566e746fa34
ajjoona <ajjoona@gmail.com>
2025-12-22 14:35:27 +0900

    fix(visualizations): URL 경로 '/' 누락본 수정

--- backend/visualizations/urls.py
+++ backend/visualizations/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("commodities/prices", views.commodity_price_series),
+    path("commodities/prices/", views.commodity_price_series),
 ]
