b7b711e55bb84b449b7f7c600f0cf1ccf5141bd9
ajjoona <ajjoona@gmail.com>
2025-12-25 02:15:05 +0900

    feat(settings): 전역 권한 설정을 IsAuthenticatedOrReadOnly으로 변경

--- backend/moathon/settings.py
+++ backend/moathon/settings.py
@@ -79,7 +79,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication', # 개발자 테스트용 (Swagger)
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
